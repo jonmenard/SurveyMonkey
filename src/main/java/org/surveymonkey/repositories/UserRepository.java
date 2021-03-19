9f19174f4a89066cada769ee5937902b6a8b470a
RDCarleton <xevoker007@gmail.com>
2021-03-18 23:45:46 -0400

    updated user repository to extend paging and sorting

--- src/main/java/org/surveymonkey/repositories/UserRepository.java
+++ src/main/java/org/surveymonkey/repositories/UserRepository.java
@@ -1,12 +1,9 @@
 package org.surveymonkey.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.transaction.annotation.Transactional;
-import org.surveymonkey.models.TextQuestion;
 import org.surveymonkey.models.User;
 
-@Transactional
-public interface UserRepository {
+public interface UserRepository extends PagingAndSortingRepository<User, Long>{
     User findById(long id);
     User findByName(String name);
 }
