947aa76ed168d2b2be9353ea82c9a8362a9cd77a
pauliwag <jake.roode@outlook.com>
2021-03-29 02:47:52 -0400

    Revamped addQuestionChoice UI

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,26 @@
 (() => {
-    $('#doSurveyTable')
-        .transition('swing down in')
-    ;
-    $('.jiggle.buttons .button')
-        .transition({
-            animation: 'jiggle',
-            duration: 800,
-            interval: 200
-        })
-    ;
-    $('select')
-        .dropdown()
-    ;
+    $(() => {
+        $('.addQuestionToSurvey')
+            .submit(() => {
+                var $q = $('#question');
+                $q.val($q.val() + '?');
+            })
+        ;
+        $('.choiceQuestionChoiceOnAddQuestionChoice')
+            .transition('drop in')
+        ;
+        $('#doSurveyTable')
+            .transition('vertical flip in')
+        ;
+        $('.jiggle.buttons .button')
+            .transition({
+                animation: 'jiggle',
+                duration: 800,
+                interval: 200
+            })
+        ;
+        $('select')
+            .dropdown()
+        ;
+    });
 });
