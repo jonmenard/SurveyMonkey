b6efefa74e66506a0091876931fe63e6c9463639
Jon <jonmenard007@gmail.com>
2021-04-02 14:41:38 -0400

    testing some logging

--- src/main/java/org/surveymonkey/Application.java
+++ src/main/java/org/surveymonkey/Application.java
@@ -1,14 +1,16 @@
 package org.surveymonkey;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 /**
  * Drives the application.
  */
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
